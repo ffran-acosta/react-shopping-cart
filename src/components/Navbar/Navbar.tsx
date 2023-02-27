f71c7f706628087c634eeea52d0f2d7cd77fe136
ffran.acosta <francoaugustoacosta@gmail.com>
2023-02-27 14:19:54 -0300

    title changed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,9 +9,9 @@ const Navbar = () => {
         <NavbarBs sticky="top" className='bg-white shadow-sm mb-3'>
             <Container>
                 <Nav className='me-auto'>
-                    <Nav.Link to={'/'} as={NavLink}>
+                    {/* <Nav.Link to={'/'} as={NavLink}>
                         Home
-                    </Nav.Link>
+                    </Nav.Link> */}
                     <Nav.Link to={'/store'} as={NavLink}>
                         Store
                     </Nav.Link>
