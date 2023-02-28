ad65917dbc8ed7b5cc2a9d8dc4dfabe84eb70898
Franco Acosta <francoaugustoacosta@gmail.com>
2023-02-28 09:52:56 -0300

    Update db.rail.way.js

--- server/data/db.rail.way.js
+++ server/data/db.rail.way.js
@@ -1,24 +1,13 @@
-// const Pool = require('pg').Pool
+ const Pool = require('pg').Pool
 
-// const pool = new Pool({
-//     user: process.env.USER,
-//     host: process.env.HOST,
-//     database: process.env.DATABASE,
-//     password: process.env.PASSWORD,
-//     port: process.env.PORT,
-// })
-
-// module.exports = pool
-
-const Pool = require('pg').Pool
-
-const pool = new Pool({
-    user: "postgres",
-    host: "containers-us-west-49.railway.app",
-    database: "railway",
-    password: "2Nw9rRVwa7J5GWy10uHM",
-    port: 7381
+ const pool = new Pool({
+     user: process.env.USER,
+     host: process.env.HOST,
+     database: process.env.DATABASE,
+     password: process.env.PASSWORD,
+     port: process.env.PORT,
 })
 
 module.exports = pool
+
 
